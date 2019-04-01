a2e9a85d299b5aaadd8b4edfe643324b3829abbd
akshaycj <akshaycj999@gmail.com>
2019-04-01 15:17:02 +0530

    Added google sign in

--- src/Components/Auth/index.js
+++ src/Components/Auth/index.js
@@ -1,12 +1,32 @@
 import React, { Component } from "react";
 import "./index.css";
-import { Icon } from "antd";
+import { Icon, message } from "antd";
+import { provider, auth } from "./../../Utils/config";
 
 export default class extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    auth.getRedirectResult().then(result => {
+      console.log("result", result);
+
+      if (result.user) {
+        message.success("Yayyy");
+      }
+    });
+  }
+
+  onSignIn = () => {
+    auth.signInWithRedirect(provider);
+  };
+
   render() {
     return (
       <div className="auth-main">
-        <div className="button">
+        <div className="button" onClick={this.onSignIn}>
           <Icon type="google" />
           Sign In
         </div>
