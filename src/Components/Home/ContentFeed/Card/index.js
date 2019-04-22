2a35cd9b20e2ff4ab30326c59550b80831447879
akshaycj <akshaycj999@gmail.com>
2019-04-22 17:13:21 +0530

    Modified UI for Content section

--- src/Components/Home/ContentFeed/Card/index.js
+++ src/Components/Home/ContentFeed/Card/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./index.css";
-import { Modal, Button, Tag } from "antd";
+import { Modal, Button, Tag, Icon } from "antd";
 import { Redirect } from "react-router-dom";
 import Collapse from "react-collapse";
 
@@ -14,8 +14,12 @@ export default class Card extends Component {
     };
   }
 
-  handleModal = () => {
+  handleExpand = () => {
     this.setState({ open: !this.state.open });
+  };
+
+  handleModal = () => {
+    this.setState({ visible: true });
   };
 
   handleCancel = () => {
@@ -30,10 +34,10 @@ export default class Card extends Component {
 
   render() {
     const { item } = this.props;
-    const { visible, redirect, open } = this.state;
+    const { redirect, open } = this.state;
     return (
-      <div className="card-container" onClick={this.handleModal}>
-        <Modal
+      <div className="card-container">
+        {/* <Modal
           visible={visible}
           onOk={this.handleOk}
           onCancel={this.handleCancel}
@@ -48,42 +52,37 @@ export default class Card extends Component {
         >
           <h1>{item.title}</h1>
           {item.description}
-        </Modal>
+        </Modal> */}
 
-        <h3>{item.title.charAt(0).toUpperCase() + item.title.slice(1)}</h3>
-        <h5 style={{ color: "rgba(0, 0, 0, 0.5)" }}>Category: Notes</h5>
+        <div className="inner-body">
+          <div>
+            <h3>{item.title.charAt(0).toUpperCase() + item.title.slice(1)}</h3>
+            <h5 style={{ color: "rgba(0, 0, 0, 0.5)" }}>Category: Notes</h5>
+          </div>
+          <div>
+            <Button type="dashed" onClick={this.handleOk}>
+              View
+            </Button>
+          </div>
+        </div>
         <Collapse isOpened={open}>
           {/* <p>
             {item.description.slice(0, 105) +
               (item.description.length > 105 ? "..." : "")}
           </p> */}
-          <div>{item.description}</div>
+          <div>Details : {item.description}</div>
         </Collapse>
-        <div>
-          <Tag
-            color="green"
-            style={{ borderStyle: "dashed", background: "#fff" }}
-          >
-            geekblue
-          </Tag>
-          <Tag
-            color="green"
-            style={{ borderStyle: "dashed", background: "#fff" }}
-          >
-            geekblue
-          </Tag>
-          <Tag
-            color="green"
-            style={{ borderStyle: "dashed", background: "#fff" }}
-          >
-            geekblue
-          </Tag>
-          <Tag
-            color="green"
-            style={{ borderStyle: "dashed", background: "#fff" }}
-          >
-            geekblue
-          </Tag>
+        <div className="tag-group">
+          <Tag>GeekBlue</Tag>
+          <Tag>GeekBlue</Tag>
+          <Tag>GeekBlue</Tag>
+        </div>
+        <div className="expand-button-container" onClick={this.handleExpand}>
+          <Icon
+            style={{ marginLeft: "auto", marginTop: "auto" }}
+            type="caret-down"
+            theme="filled"
+          />
         </div>
         {/* <button className="button-openfile">
           <Icon type="download" />
